7ead3572661c7619a0f2dca0ce98623cfff5402d
Terry Kim <terrykim950@gmail.com>
2021-12-08 02:53:51 -0500

    cleaned searchbox code

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -8,6 +8,7 @@ import { AiOutlineClockCircle } from "@react-icons/all-files/ai/AiOutlineClockCi
 import Typing from "react-typing-animation";
 import ReactTooltip from "react-tooltip";
 
+// component containing typing animation
 const WelcomeMessage = React.memo(() => {
   return (
     <Typing className="welcome-message">
@@ -21,52 +22,84 @@ const WelcomeMessage = React.memo(() => {
   );
 });
 
+// component containing search icon, input, and mic icon
+const SearchBoxInput = ({
+  setFocused,
+  pages,
+  setPageSuggestions,
+  handleSubmit,
+}) => {
+  return (
+    <div className="input-container" id="search-tour">
+      <AiOutlineSearch size={20} />
+      <input
+        className="input"
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        onChange={(event) => {
+          setPageSuggestions(
+            pages.filter((page) =>
+              page.toLowerCase().includes(event.target.value.toLowerCase())
+            )
+          );
+        }}
+        onKeyPress={handleSubmit}
+      />
+      <Link to="/voice-search">
+        <BiMicrophone size={20} color="black" data-tip="Search by voice" />
+        <ReactTooltip place="bottom" effect="solid" />
+      </Link>
+    </div>
+  );
+};
+
+// main component
 const SearchBox = ({ stepsEnabled, setStepsEnabled }) => {
   const pages = ["About Me", "Adventure", "Project", "Resume"];
 
   const [pageSuggestions, setPageSuggestions] = useState(pages);
   const [typing, setTyping] = useState(true);
+  const [focused, setFocused] = useState(false);
 
   const handleSubmit = (event) => {
     if (event.key === "Enter") {
       if (pageSuggestions[0])
-        navigate(`/${pageSuggestions[0].toLowerCase().replace(" ", "")}`);
+        navigate(`/${pageSuggestions[0].toLowerCase().replace(" ", "-")}`);
       else navigate("/404");
     }
   };
 
   return (
-    <div
-      className="search-box-container"
-      onClick={() => setTyping(false)}
-      onKeyDown={() => setTyping(false)}
-      role="button"
-      tabIndex="0"
-    >
-      <div className="search-box-inner-container" id="search-tour">
-        <AiOutlineSearch size={20} />
-        <div className="input-container">
-          {typing ? <WelcomeMessage /> : null}
-          <input
-            className="search-input"
-            type="text"
-            onChange={(event) => {
-              setPageSuggestions(
-                pages.filter((page) =>
-                  page.toLowerCase().includes(event.target.value.toLowerCase())
-                )
-              );
-            }}
-            onKeyPress={handleSubmit}
-          />
+    <div className="container">
+      <div
+        className="search-box-container"
+        onClick={() => setTyping(false)}
+        onKeyDown={() => setTyping(false)}
+        role="button"
+        tabIndex="0"
+      >
+        <SearchBoxInput
+          setFocused={setFocused}
+          setPageSuggestions={setPageSuggestions}
+          handleSubmit={handleSubmit}
+          pages={pages}
+        />
+        {typing ? <WelcomeMessage /> : null}
+        <div className="history-container">
+          <div className="horizontal-rule"></div>
+          {pageSuggestions.map((page, i) => (
+            <Link
+              className="history"
+              key={i}
+              to={`/${page.toLowerCase().replace(" ", "-")}`}
+            >
+              <AiOutlineClockCircle color="grey" />{" "}
+              <span className="history-text">{page}</span>
+            </Link>
+          ))}
         </div>
-        <Link to="/voice-search">
-          <BiMicrophone size={20} color="black" data-tip="Search by voice" />
-          <ReactTooltip place="bottom" effect="solid" />
-        </Link>
       </div>
-
-      <div className="buttons-container">
+      {!focused ? (
         <div
           className="instruction-button"
           onClick={() => setStepsEnabled(!stepsEnabled)}
@@ -76,23 +109,65 @@ const SearchBox = ({ stepsEnabled, setStepsEnabled }) => {
         >
           How to Use
         </div>
-      </div>
-
-      <div className="history-container">
-        <div className="horizontal-rule"></div>
-
-        {pageSuggestions.map((page, i) => (
-          <Link
-            className="history"
-            key={i}
-            to={`/${page.toLowerCase().replace(" ", "-")}`}
-          >
-            <AiOutlineClockCircle color="grey" />{" "}
-            <span className="history-text">{page}</span>
-          </Link>
-        ))}
-      </div>
+      ) : null}
     </div>
+    // <div
+    //   className="search-box-default"
+    // onClick={() => setTyping(false)}
+    // onKeyDown={() => setTyping(false)}
+    // role="button"
+    // tabIndex="0"
+    // >
+    //   <div className="search-box-inner-container" id="search-tour">
+    //     <AiOutlineSearch size={20} />
+    //     <div className="input-container">
+    //       {typing ? <WelcomeMessage /> : null}
+    //       <input
+    //         className="search-input"
+    //         type="text"
+    //         onChange={(event) => {
+    //           setPageSuggestions(
+    //             pages.filter((page) =>
+    //               page.toLowerCase().includes(event.target.value.toLowerCase())
+    //             )
+    //           );
+    //         }}
+    //         onKeyPress={handleSubmit}
+    //       />
+    //     </div>
+    //     <Link to="/voice-search">
+    //       <BiMicrophone size={20} color="black" data-tip="Search by voice" />
+    //       <ReactTooltip place="bottom" effect="solid" />
+    //     </Link>
+    //   </div>
+
+    //   <div className="buttons-container">
+    //     <div
+    //       className="instruction-button"
+    //       onClick={() => setStepsEnabled(!stepsEnabled)}
+    //       onKeyDown={() => setStepsEnabled(!stepsEnabled)}
+    //       tabIndex="0"
+    //       role="button"
+    //     >
+    //       How to Use
+    //     </div>
+    //   </div>
+
+    //   <div className="history-container">
+    //     <div className="horizontal-rule"></div>
+
+    //     {pageSuggestions.map((page, i) => (
+    //       <Link
+    //         className="history"
+    //         key={i}
+    //         to={`/${page.toLowerCase().replace(" ", "-")}`}
+    //       >
+    //         <AiOutlineClockCircle color="grey" />{" "}
+    //         <span className="history-text">{page}</span>
+    //       </Link>
+    //     ))}
+    //   </div>
+    // </div>
   );
 };
 
