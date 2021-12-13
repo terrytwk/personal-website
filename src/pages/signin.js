ef041d3b0c1e456c42a553b3a4ea01d41650aba7
Terry Kim <terrykim950@gmail.com>
2021-12-13 02:22:49 -0500

    connected api

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -3,18 +3,48 @@ import "../css/signin.css";
 import terry_logo from "../images/terry_logo.png";
 
 import TextField from "@mui/material/TextField";
+import { AiOutlineCheckCircle } from "@react-icons/all-files/ai/AiOutlineCheckCircle";
 
 const Signin = () => {
   const [submitted, setSubmitted] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const [name, setName] = useState("");
   const [contactInfo, setContactInfo] = useState("");
   const [comments, setComments] = useState("");
 
   const handleSubmit = () => {
-    // add loading animation on button
-    alert(`${name} with contact info ${contactInfo} has said "${comments}"`);
-    setSubmitted(true);
+    if (!name && !contactInfo && !comments)
+      return setErrorMessage("At least one field is required.");
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append(
+      "Cookie",
+      "COMPASS=spreadsheet_forms=CjIACWuJVwzzJOgKRV7ZE-eGY8Cz_5qovxPZt5nPgy-cNSBmkvZo4M8KsYsTYHGEU2MQnhCN8tuNBho0AAlriVctfIS1X2wSCQdpNlFwKG1A2jbAcaQVvao6bZ8VVVKV0Asqpa6MtTN_NIOCESoXmA==; S=spreadsheet_forms=1BIBbdvVfUaQP09-x7eGNOR9xRrbnmOp3_WKYbEjJz8; NID=511=GoGE_eUzcXXnU9mU7a19tPjpm7M7r9E5OD73tHAjnwJ9RvhbH76kXEJ-IwEWLd7nBoC4-iw-SpMdYRmTnuSo2wfHEqfEbHEebQ6DVfGpQUYDe8U4DUwNZGxMh6zilo1VKh_FSNKKKHebuApWVnTBUjwSOQyZTbQMV6lW__sp3fw"
+    );
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("entry.1774825969", name);
+    urlencoded.append("entry.1421939944", contactInfo);
+    urlencoded.append("entry.1638789278", comments);
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://docs.google.com/forms/u/0/d/e/1FAIpQLSf9cJpzVlIOXc6lQXEvqvwVZkRcnJws61UbAznuf2WPnnneIg/formResponse",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => setSubmitted(true))
+      .catch((error) =>
+        setErrorMessage("Something went wrong. Please try again.")
+      );
   };
 
   return (
@@ -48,7 +78,8 @@ const Signin = () => {
               value={comments}
               onChange={(event) => setComments(event.target.value)}
             />
-            <div
+            <p className="error-message">{errorMessage}</p>
+            <button
               className="submit"
               onClick={handleSubmit}
               onKeyDown={handleSubmit}
@@ -56,10 +87,13 @@ const Signin = () => {
               tabIndex="0"
             >
               Submit
-            </div>
+            </button>
           </>
         ) : (
-          <span className="submitted-message">Message has been sent!</span>
+          <span className="submitted-message">
+            <AiOutlineCheckCircle color="green" size="25}" />
+            <span className="text">Message has been sent!</span>
+          </span>
         )}
       </div>
     </div>
