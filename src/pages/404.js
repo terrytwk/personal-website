4b2cd9716314015ae715740f7b4511681a21053c
Terry Kim <terrykim950@gmail.com>
2021-12-22 23:26:53 -0800

    added a comment

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,28 +1,34 @@
 import * as React from "react";
 import "../css/404.css";
-import { Link } from "gatsby";
 
 import SearchBoxHeader from "../components/SearchBoxHeader";
 
-const NotFoundPage = ({ location }) => {
-  return (
-    <div className="not-found-page-container search-page">
-      <SearchBoxHeader />
-      <div className="search-content">
-        <p>
-          Your search - {location.state.searchString} - did not match any
-          documents.
-        </p>
-        <p>Suggestions:</p>
-        <ul>
-          <li>About Me</li>
-          <li>Adventure</li>
-          <li>Project</li>
-          <li>Resume</li>
-        </ul>
+const NotFoundPage = ({
+  location = { state: { searchString: "your searched word" } },
+}) => {
+  // to fix build time error of location.state undefined
+  if (location.state) {
+    return (
+      <div className="not-found-page-container search-page">
+        <SearchBoxHeader />
+        <div className="search-content">
+          <p>
+            Your search - {location.state.searchString} - did not match any
+            documents.
+          </p>
+          <p>Suggestions:</p>
+          <ul>
+            <li>About Me</li>
+            <li>Adventure</li>
+            <li>Project</li>
+            <li>Resume</li>
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 
 export default NotFoundPage;
