05482088c476fbcb90fabee377a1dc1573bf3933
Terry Kim <terrykim950@gmail.com>
2022-01-24 18:27:30 -0700

    added metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,11 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://terrytwk.github.io/",
     title: "terrytwk.github.io",
+    description: "Personal website for Tae Wook (Terry) Kim",
+    author: "Terry Kim",
+    keywords:
+      "Terry, Tae Wook, Tae, Wook, Taewok, Terry Kim, Tae Wook Kim, Kim",
   },
   plugins: [
     {
